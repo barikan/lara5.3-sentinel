fcb446ee42de0f8d4c1be43390c28b29df8cfda0
Toshihiro Hiraoka <hiraoka@azukimap.jp>
2018-01-14 02:25:32 +0900

    * Removed asset.js and place static js files for modal strange behaviours in admin pages.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,10 +92,12 @@
     </div>
 
     <!-- Scripts -->
+    <!--
+    <script type="text/javascript" src="./js/common-asset.js"></script>
     @yield('scripts')
+    -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </body>
-
-<script type="text/javascript" src="./js/common-asset.js"></script>
-<script type="text/javascript" src="./js/app.js"></script>
-
 </html>
